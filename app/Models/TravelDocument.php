f9355400fdff04eaca57e22d382c6efe8552969e
MysticMaccc <cosmicsher96@gmail.com>
2025-10-01 17:48:08 +0800

    created travel document model

---
+++ app/Models/TravelDocument.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasModifiedBy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TravelDocument extends Model
+{
+    use SoftDeletes, HasModifiedBy;
+
+    protected $fillable = [
+        'crew_id',
+        'id_no',
+        'travel_document_type_id',
+        'place_of_issue',
+        'date_of_issue',
+        'expiration_date',
+        'remaining_pages',
+    ];
+
+    protected $casts = [
+        'date_of_issue' => 'date',
+        'expiration_date' => 'date',
+        'remaining_pages' => 'integer',
+    ];
+
+    public function crew(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'crew_id');
+    }
+
+    public function travelDocumentType(): BelongsTo
+    {
+        return $this->belongsTo(TravelDocumentType::class);
+    }
+}
